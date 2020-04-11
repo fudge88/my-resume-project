b31ca25414d48c5ff40117ef3512583685963c7f
fudge88 <f.akhlaq@outlook.com>
2020-04-11 20:44:25 +0000

    github usrname writing added

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,4 +1,4 @@
-function fetchGithubInformation(event) {
+function fetchGitHubInformation(event) {
     var username= $("gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
