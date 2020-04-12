9c379af7f7d76f6c740f89b6e49df2dbffc6f73f
fudge88 <f.akhlaq@outlook.com>
2020-04-12 15:23:51 +0000

    emails not linking- trying new code

--- assets/js/contact.js
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/emailjs-com@2.3.2/dist/email.min.js"></script>
-<script type="text/javascript">
-   (function(){
-      emailjs.init("user_nSvDfzE3Zj0etYWKO8d8D");
-      emailjs.send("gmail", "Resume", {name: "Fahra", notes: "The project that i want is..."});
-   })();
-</script>
